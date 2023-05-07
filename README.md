5ce0eb4f5019979f88260a09e9b51ae15f9aef8c
Casey Rogers <caseycrogers@berkeley.edu>
2023-05-06 22:28:32 -0700

    expanded on readme

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-Ensure all gaps and margins between you widgets are consistent and
-adapt to screen size!
+Ensure all visual gaps between you widgets are consistent, adapted to the axis direction, and respond to screen size!
+
+## Core Features
+
+Provides a `Gutter` and `Margin` that correspond to Material
+Design's [Responsive Layout system](https://m2.material.io/design/layout/responsive-layout-grid.html#columns-gutters-and-margins)
+.
+
+These classes:
+
+1. Are sized according to Material design's adaptive break points
+2. Find their nearest `Scrollabe`, `Column` or `Row` ancestor and create a vertical gap for vertical ancestors,
+   horizontal for horizontal ancestors
+
+For more flexibility, you can also use an extension on `BuildContext` to reference the gutter and margin sizes
+directly: `context.gutter`, `context.margin`.
+
+`GutterSmall`, `GutterTiny` and `GutterLarge` all provide gaps that are factors of the base gutter size for situations
+where larger or smaller gaps are more appropriate.
+
+## Example
+
+Without Gutter
+```dart
+return Column(
+  chilren: [
+    const Text('I hate using sized box.'),
+    // I have to manually specify that I want a vertically aligned gap.
+    // And I have to manually set an arbitrary size for the gap.
+    SizedBox(height: 10),
+    const Text('If only there were a better way...'),
+  ],
+);
+```
+
+### With Gutter
+```dart
+return Column(
+  chilren: [
+    const Text('I hate using sized box.'),
+    // Gutter identified that it's nearest relevant ancestor was a column, so it creates a vertical gap!
+    // It expands and contracts according to Material Design's adaptive breakpoint definitions so it looks good on all screen sizes!
+    Gutter(),
+    const Text('And now I don\'t have to!'),
+  ],
+);
+```
---
+++ example/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:gutter/gutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      // Get the margin size from context and pad the page contents by it.
+      body: Padding(
+        padding: EdgeInsets.all(context.margin),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('Button tapped'),
+              const GutterSmall(),
+              Text(
+                _counter.toString(),
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+              const GutterSmall(),
+              const Text('times'),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
